218c88bcb486784bac6433c53dbfc62bec4baaba
Carl Pearson <cwpears@sandia.gov>
2023-12-07 10:00:02 -0800

    perlmutter fixes

--- perlmutter-a100/run.sh
+++ perlmutter-a100/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+#SBATCH  -A m3953
+#SBATCH -C "gpu&hbm40g"
+#SBATCH  -G 1
+#SBATCH --qos regular
+#SBATCH -N 1
+#SBATCH -t 60
 
 set -eou pipefail
 
-srun -A m3953 -C "gpu&hbm40g" -G 1 --qos regular -N 1 -t 120 ./run-worker.sh
+srun -A m3953 -C "gpu&hbm40g" -G 1 --qos regular -N 1 -t 60 ./run-worker.sh
