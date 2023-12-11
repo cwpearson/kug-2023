63f79d7484649ffb0f844dce87d5dc98dd37f260
Carl Pearson <cwpears@sandia.gov>
2023-12-11 10:02:12 -0700

    add another matrix

--- blake-h100-cuda12/run.sh
+++ blake-h100-cuda12/run.sh
@@ -17,6 +17,7 @@ srun -n1 -t 1 cat /proc/cpuinfo |& tee "$LOG_DIR/cpuinfo.log" || true
 srun -n1 -t 1 env |& tee "$LOG_DIR/env.log" || true
 
 
-srun -N 1 -p H100 -n 1 -t 60 ctest --test-dir "$KERNELS_BUILD" |& tee "$LOG_DIR/ctest.log"
+srun -N 1 -p H100 -n 1 -t 60 "$KERNELS_BUILD"/perf_test/sparse/KokkosKernels_sparse_spmv_benchmark -f /projects/cwpears/kug-2023/dielFilterV3real/dielFilterV3real.mtx |& tee "$LOG_DIR/spmv2.log"
 srun -N 1 -p H100 -n 1 -t 60 "$KERNELS_BUILD"/perf_test/blas/blas3/KokkosKernels_Blas3_gemm_benchmark --cuda 0 |& tee "$LOG_DIR/gemm.log"
-srun -N 1 -p H100 -n 1 -t 60 "$KERNELS_BUILD"/perf_test/sparse/KokkosKernels_sparse_spmv_benchmark -f /projects/cwpears/sparc_gpu_problems/single_gpu/matrix.mm |& tee "$LOG_DIR/spmv.log"
+srun -N 1 -p H100 -n 1 -t 60 "$KERNELS_BUILD"/perf_test/sparse/KokkosKernels_sparse_spmv_benchmark -f /projects/cwpears/sparc_gpu_problems/single_gpu/matrix.mm |& tee "$LOG_DIR/spmv1.log"
+srun -N 1 -p H100 -n 1 -t 60 ctest --test-dir "$KERNELS_BUILD" |& tee "$LOG_DIR/ctest.log"
